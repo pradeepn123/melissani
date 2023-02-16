d672e54bb0cea5dd1c171df7d3d0e49a2447810f
darshika-dev <darshika@shoptrade.co>
2023-02-17 01:18:26 +0530

    Changes in banner and features section

--- app/routes/($lang)/index.jsx
+++ app/routes/($lang)/index.jsx
@@ -1,7 +1,7 @@
 import {defer} from '@shopify/remix-oxygen';
 import {Suspense} from 'react';
 import {Await, useLoaderData} from '@remix-run/react';
-import {ProductSwimlane, FeaturedCollections, Hero} from '~/components';
+import {ProductSwimlane, FeaturedCollections, Hero, KeyFeatures} from '~/components';
 import {MEDIA_FRAGMENT, PRODUCT_CARD_FRAGMENT} from '~/data/fragments';
 import {getHeroPlaceholder} from '~/lib/placeholders';
 import {AnalyticsPageType} from '@shopify/hydrogen';
@@ -175,7 +175,10 @@ export default function Homepage() {
       )}
 
       {goodbye && (
-        <ImageWithText goodbye={goodbye} height="full"  />
+        <ImageWithText goodbye={goodbye} height="full"  />)}
+
+      {features && (
+        <KeyFeatures features={features} />
       )}
 
       {featuredProducts && (
