0882613c61bdf887581471d6d18d35e76b870993
darshika-dev <darshika@shoptrade.co>
2023-02-28 12:30:40 +0530

    Fixed conflicts

--- app/routes/($lang)/index.jsx
+++ app/routes/($lang)/index.jsx
@@ -200,6 +200,7 @@ export default function Homepage() {
       {videoSection && (
         <VideoPlayer data={videoSection} />
       )}
+      {discover && (<ImageWithText discover={discover} discoverClassName={"flex discover-main-section"} />)}
 
       {installation && (<ImageWithTwoText installation={installation} />)}
 
