9f1c60509993edc7a9fa113eb8a3789be3f67a89
Sumayya Ishaq <sumayya@shoptrade.co>
2023-03-06 12:56:05 +0530

    [MEL-5271118] Added FAQ Accordion as a component

--- app/components/Faq/Faq.jsx
+++ app/components/Faq/Faq.jsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'flowbite-react';
-import { Accordion } from 'flowbite-react';
+import { FaqAccordion } from './FaqAccordion';
 
 export function Faq({data}) {
     return (
@@ -12,20 +12,7 @@ export function Faq({data}) {
                     >
                     {(data?.tab_heading || []).map((item, index) => (
                         <Tabs.Item key={`faq-${index}`} active={true} title={<div className='flex flex-col text-sm items-center text-[#1376BC] icon_with_text'><img src={item.icon} alt=''/><p className='font-sfpro font-extrabold tracking-wider text-base text-[#1376BC]'>{item.text}</p></div>}>
-                            <Accordion flush={true} className="w-8/12 pt-14">
-                                {(item?.contentinfo || []).map((content, index) => (
-                                    <Accordion.Panel key={`faq-${index}`}>
-                                        <Accordion.Title>
-                                            <h2 className='text-3xl font-sfpro w-4/5 font-text-28 font-bold text-[#000000]'>{content.question}</h2>
-                                        </Accordion.Title>
-                                        <Accordion.Content className='custom_content'>                                        
-                                            <p className="font-normal font-sfpro text-2xl font-text-22 text-[#000000]">
-                                                {content.answer}
-                                            </p>
-                                        </Accordion.Content>
-                                    </Accordion.Panel>
-                                ))}
-                            </Accordion>
+                            <FaqAccordion item={item} />
                         </Tabs.Item>
                     ))}
                 </Tabs.Group>
