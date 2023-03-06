ad2ddf1591594d97d38626fd344b0b89e2fd833f
Pradeepkumar Neginhal <shoptradepradeep@Pradeepkumars-MacBook-Pro.local>
2023-03-06 16:33:46 +0530

    About us mobile layout fixes

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -5,13 +5,13 @@ export function About({data}) {
             <div className="image_main_wrapper">
                 <img className="w-full" src={data.image_url_desktop} alt=""/>
             </div>
-            <div className="content_info_section mx-5 px-6 md:mx-10 md:px-8 lg:px-12">
+            <div className="content_info_section mx-5 md:mx-10 px-5 md:px-10">
                 {(data?.about_info || []).map((block, index) => (
                     <div className={`about_us_content xl:absolute ${index % 2 == 0 ? 'left' : 'right'}`}>
                         <div key={`about-${index}`} className="about_us_content_inner">
-                            <h2 className="uppercase font-bold pb-2">{block.heading}</h2>
-                            <h4 className="pb-2">{block.sub_heading}</h4>
-                            <p className="">{block.content}</p>
+                            <h2 className="font-primary uppercase font-bold pb-2">{block.heading}</h2>
+                            <h4 className="font-secondary pb-2">{block.sub_heading}</h4>
+                            <p className="font-tertiary">{block.content}</p>
                         </div>
                     </div>
                 ))}
