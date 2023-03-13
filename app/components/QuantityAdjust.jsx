ca20eaeae05df78e5e5217fa343af89ddc65e20b
Sumayya Ishaq <sumayya@shoptrade.co>
2023-03-13 15:48:38 +0530

    Added product form

---
+++ app/components/QuantityAdjust.jsx
@@ -0,0 +1,39 @@
+export function QuantityAdjust({quantity, setQuantity}) {
+  const prevQuantity = Number(Math.max(0, quantity - 1).toFixed(0));
+  const nextQuantity = Number((quantity + 1).toFixed(0));
+    
+  return (
+    <>
+      <div className="flex items-center border quantity-selector">
+        <div>
+          <button
+            name="decrease-quantity"
+            aria-label="Decrease quantity"
+            className="w-10 h-6 transition text-black hover:text-primary disabled:text-primary/10"
+            value={prevQuantity}
+            disabled={quantity <= 1}
+            onClick={() => setQuantity(prevQuantity)}
+          >
+            <span>&#8722;</span>
+          </button>
+        </div>
+
+        <div className="px-2 text-center text-black" data-test="item-quantity">
+          {quantity}
+        </div>
+
+        <div>
+          <button
+            className="w-10 h-6 transition text-black hover:text-primary"
+            name="increase-quantity"
+            value={nextQuantity}
+            onClick={() => setQuantity(nextQuantity)}
+            aria-label="Increase quantity"
+          >
+            <span>&#43;</span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
