ea44c549d4af081024d11026278be798ecac192c
Sumayya Ishaq <sumayya@shoptrade.co>
2023-03-14 17:42:20 +0530

    Separated product components

---
+++ app/components/ProductDescription/ProductDescription.jsx
@@ -0,0 +1,35 @@
+import {
+  Money
+} from '@shopify/hydrogen';
+import {
+  Heading,
+  ProductForm
+} from '~/components';
+
+export function ProductDescription ({ isSubscriptionProduct, title, selectedVariant, parsedProductDetails }) {
+  return (<>
+    {isSubscriptionProduct ?
+      <div></div> : 
+      <div className="product-form-wrapper">
+        <div className="grid gap-2">
+          <Heading as="h1" className="whitespace-normal product-title">
+            {title}
+          </Heading>
+          {selectedVariant && <div className="product-price font-tertiary">
+            <Money
+            withoutTrailingZeros
+            data={selectedVariant?.price}
+            as="span"
+            />
+          </div>}
+        </div>
+        <ProductForm />
+        <div className="grid gap-4 py-4">
+          {parsedProductDetails?.productDescription && (
+            <div className="desc-list" dangerouslySetInnerHTML={{ __html: parsedProductDetails.productDescription}}></div>
+          )}
+        </div>
+      </div>
+    }
+  </>)
+}
---
+++ app/components/ProductDescription/ProductOptionLink.jsx
@@ -0,0 +1,36 @@
+import {
+  useLocation
+} from '@remix-run/react';
+import {
+  Link
+} from '~/components';
+
+export function ProductOptionLink({
+  optionName,
+  optionValue,
+  searchParams,
+  children,
+  ...props
+}) {
+  const {pathname} = useLocation();
+  const isLangPathname = /\/[a-zA-Z]{2}-[a-zA-Z]{2}\//g.test(pathname);
+  // fixes internalized pathname
+  const path = isLangPathname
+    ? `/${pathname.split('/').slice(2).join('/')}`
+    : pathname;
+
+  const clonedSearchParams = new URLSearchParams(searchParams);
+  clonedSearchParams.set(optionName, optionValue);
+
+  return (
+    <Link
+      {...props}
+      preventScrollReset
+      prefetch="intent"
+      replace
+      to={`${path}?${clonedSearchParams.toString()}`}
+    >
+      {children ?? optionValue}
+    </Link>
+  );
+}
