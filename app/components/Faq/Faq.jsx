60bf58b6cfc447de5d1c011061df315ebadef700
Pradeepkumar Neginhal <shoptradepradeep@Pradeepkumars-MacBook-Pro.local>
2023-03-17 12:58:36 +0530

    Faq page mobile layout css changes

--- app/components/Faq/Faq.jsx
+++ app/components/Faq/Faq.jsx
@@ -14,13 +14,13 @@ export function Faq({data}) {
             <div className="container mx-auto">
                 <h2 className="main_content_heading font-bold pb-16">{data.heading}</h2>
                 <Tabs.Group className='flex justify-between tabs_with_icons'
-                    aria-label="Tabs with icons"                    
+                    aria-label="Tabs with icons"
                     >
                     {(data?.tab_heading || []).map((item, index) => (
                         <Tabs.Item key={`faq-${index}`} active={true} title={
                             <div className='flex flex-col text-sm items-center text-[#1376BC] icon_with_text'>
                                 <img src={item.icon} alt=''/>
-                                <p className='font-sfpro font-extrabold tracking-wider text-base text-[#1376BC]'>{item.text}</p>
+                                <p className='tab__title font-extrabold text-base text-[#1376BC]'>{item.text}</p>
                             </div>
                         }>
                             <FaqAccordion item={item} />
