8458406b977241ef728e2611616f79901ecfd488
hriks <hriks@outlook.com>
2023-03-25 22:01:33 +0530

    Able to add Purifier from Sticky Add to cart button

--- app/components/ProductStickyBar.jsx
+++ app/components/ProductStickyBar.jsx
@@ -1,8 +1,12 @@
 import {useEffect, useState} from 'react';
-import {Button} from '~/components';
+import {
+    Button,
+    AddToCartButton
+} from '~/components';
 import {Money} from '@shopify/hydrogen';
 
-export function ProductStickyBar({title, media, price, isSubscriptionProduct}) {
+
+export function ProductStickyBar({title, media, price, isSubscriptionProduct, ...props}) {
     const [isSticky, setIsSticky] = useState(false);
 
     useEffect(() => {
@@ -30,6 +34,7 @@ export function ProductStickyBar({title, media, price, isSubscriptionProduct}) {
     }, [isSticky])
 
     if (isSubscriptionProduct) {
+        // Handle Subscriptions line items here
         return <div className="md:hidden" id='js_stickybar_main_section'>
             <div className="stickybar_main_section">
                 <div className="container mx-auto">
@@ -49,34 +54,34 @@ export function ProductStickyBar({title, media, price, isSubscriptionProduct}) {
         </div>
     }
 
-    return(
-        <>     
-            <div className="" id='js_stickybar_main_section'>
-                <div className="stickybar_main_section">
-                    <div className="container mx-auto">
-                        <div className="stickybar_main_section_inner">
-                            <div className="sticky_image_wrapper">
-                                <img className='hidden md:block' src="https://cdn.shopify.com/s/files/1/0684/3023/3888/products/Group7192.jpg" alt="" />
-                                <div className="title_price_combo">
-                                    <h2>{title}</h2>
-                                    {price && <div className="product-price font-tertiary">
-                                        <Money
-                                            withoutTrailingZeros
-                                            data={price}
-                                            as="span"
-                                        />
-                                    </div>}
-                                </div>
-                            </div>
-                            <div className="sticky_button">
-                                <a href="">
-                                    <Button className="rounded-full text-center py-3 px-9 border border-primary bg-primary text-[#ffffff]">Add to cart</Button>
-                                </a>
-                            </div>
+    return <div className="" id='js_stickybar_main_section'>
+        <div className="stickybar_main_section">
+            <div className="container mx-auto">
+                <div className="stickybar_main_section_inner">
+                    <div className="sticky_image_wrapper">
+                        <img className='hidden md:block' src="https://cdn.shopify.com/s/files/1/0684/3023/3888/products/Group7192.jpg" alt="" />
+                        <div className="title_price_combo">
+                            <h2>{title}</h2>
+                            {price && <div className="product-price font-tertiary">
+                                <Money
+                                    withoutTrailingZeros
+                                    data={price}
+                                    as="span"
+                                />
+                            </div>}
                         </div>
+                    </div>
+                    <div className="sticky_button">
+                        <AddToCartButton
+                            className="rounded-full text-center py-3 px-9 border border-primary bg-primary text-[#ffffff]"
+                            lines={[{merchandiseId: props.selectedVariant.id, quantity: 1}]}
+                            data-test="add-to-cart"
+                        >
+                            Add to cart
+                        </AddToCartButton>
                     </div>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    </div>
 }
