8786928afe8fea424108e1e217decb27e4cd48e0
hriks <hriks@outlook.com>
2023-03-27 15:31:42 +0530

    Update login text

--- app/routes/($lang)/account/__public/login.jsx
+++ app/routes/($lang)/account/__public/login.jsx
@@ -16,7 +16,7 @@ export async function loader({context, params}) {
   }
 
   // TODO: Query for this?
-  return json({shopName: 'Hydrogen'});
+  return json({shopName: 'Melissani'});
 }
 
 const badRequest = (data) => json(data, {status: 400});
