88f78a500fb23a29eb84bd07178d9d56f91cd5bb
Sumayya Ishaq <sumayya@shoptrade.co>
2023-03-30 17:27:50 +0530

    Fixed UI for filter page

--- app/components/Carousel/Carousel.jsx
+++ app/components/Carousel/Carousel.jsx
@@ -2,7 +2,9 @@ import Flickity from 'react-flickity-component';
 import { motion } from 'framer-motion';
 
 export function Carousel({ data, className }) {
-  return (
+    var flkty = null;
+
+    return (
     <section className={`${className ? 'carousel-wrapper ' + className : 'carousel-wrapper'}`} id={className}>
         <motion.div
         initial={{ opacity: 0, transform: "translateY(60px)" }}
@@ -15,7 +17,11 @@ export function Carousel({ data, className }) {
         }}
         exit={{ opacity: 0, transform: "translateY(60px)" }}>
             <Flickity
-                pageDots="false">
+                flickityRef={c => flkty = c}
+                options={{
+                    prevNextButtons: false
+                }}
+            >
                 {data && data.map((item, index) =>
                     <div className="slider" key={index}>
                         <div className="left-slider-content">
@@ -28,13 +34,25 @@ export function Carousel({ data, className }) {
                             <p className="slider-description font-tertiary">
                                 {item.description}
                             </p>
+                            <div className="flickity-button-wrapper">
+                                <button className="flickity-button flickity-prev-next-button previous" onClick={() => flkty.previous()} disabled={index == 0}>
+                                    <svg className="flickity-button-icon" viewBox="0 0 100 100">
+                                        <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" className="arrow"></path>
+                                    </svg>
+                                </button>
+                                <button className="flickity-button flickity-prev-next-button next" onClick={() => flkty.next()} disabled={index == data.length -1}>
+                                    <svg className="flickity-button-icon" viewBox="0 0 100 100">
+                                        <path d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z" className="arrow" transform="translate(100, 100) rotate(180)"></path>
+                                    </svg>
+                                </button>
+                            </div>
                         </div>
                         <div className="right-slider-content">
                             <img className="slider-image" src={item.image} alt={item.heading}/>
                         </div>
                     </div>
-                    )}
+                )}
             </Flickity>
         </motion.div>
-      </section>
+    </section>
 )}
