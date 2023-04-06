9757fc79cbd5c03c81ef22c88b141d192b20289f
shivanshi0526 <shivanshi0526@gmail.com>
2023-04-06 15:50:31 +0530

    css for 404 page

--- app/components/NotFound.jsx
+++ app/components/NotFound.jsx
@@ -1,22 +1,27 @@
-import {Button} from './Button';
 import {FeaturedSection} from './FeaturedSection';
 import {PageHeader, Text} from './Text';
+import {Button, Link} from '~/components';
 
 export function NotFound({type = 'page'}) {
-  const heading = `We’ve lost this ${type}`;
+  const heading = `404`;
   const description = `We couldn’t find the ${type} you’re looking for. Try checking the URL or heading back to the home page.`;
-
+  const subHeading = `Oops! We couldn't find the page you were looking for.`
   return (
     <>
-      <PageHeader heading={heading}>
+      <PageHeader heading={heading} className="not_found_header">
+        <Text width="narrow" as="h2">
+           {subHeading}
+        </Text>
         <Text width="narrow" as="p">
           {description}
         </Text>
-        <Button width="auto" variant="secondary" to={'/'}>
-          Take me to the home page
-        </Button>
+        <Link
+            to={'/'}>
+            <Button variant='primary' className="font-medium">
+            Back to home
+            </Button>
+        </Link>
       </PageHeader>
-      <FeaturedSection />
     </>
   );
 }
