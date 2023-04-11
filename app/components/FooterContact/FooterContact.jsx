f700d4ca6bda639811b6af6b3ea553bb2b4d68e0
darshika-dev <darshika@shoptrade.co>
2023-04-11 15:17:48 +0530

    Footer updated

--- app/components/FooterContact/FooterContact.jsx
+++ app/components/FooterContact/FooterContact.jsx
@@ -2,15 +2,15 @@ export function FooterContact({data}) {
   return (
     <div className="footer-image-section w-full pt-8 px-6 lg:pt-16 md:px-8 lg:px-12 md:flex">
         <div className="footer-text flex item-center flex-col md:w-6/12">
-          <p>
+          <p className="footer-heading">
             {data?.heading}
-            <span className='footer-subHeading leading-tight font-tertiary'>{data?.subHeading}</span>
           </p>
           <div className="footer-contact-wrapper flex mt-7">
             {data?.contact.map((item, index) => (
-              <div key={`footer-${index}`} className="footer-contact-icons mr-7">
-                <a href={item.link}>
-                  <img src={item.icon} width="85" alt="" />
+              <div key={`footer-${index}`} className="footer-contact-icons">
+                <a href={item.link} className="footer-icon-text-wrapper">
+                  <span className="footer-icon-wrapper"><img src={item.icon} width="85" alt="" /></span>
+                  <p className="footer-icon-text">{item.iconText}</p>
                 </a>
               </div>
             ))}
