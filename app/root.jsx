86ad6b93ce59473a8a3f7d69fe9e128df9a4018a
Mohammadu Mifras <30581135+mohammadumifras@users.noreply.github.com>
2023-04-18 12:34:11 -0400

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -98,7 +98,6 @@ export default function App() {
         {/* <script id="gorgias-chat-widget-install-v2" src="https://config.gorgias.chat/gorgias-chat-bundle-loader.js?applicationId=28865"></script> */}
         <script async src="https://d3hw6dc1ow8pp2.cloudfront.net/reviews-widget-plus/js/okendo-reviews.js"></script>
         <script async type="text/javascript" src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=Wp4Lww"></script>
-        <script>!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');twq('config','oenmr');</script>
       </head>
       <body>
         <Layout
