a4ff203cd4da847ece9cf02a6d5c2273bdce1948
Keerthi-Sindhura <keerthi@shoptrade.co>
2023-05-10 17:53:59 +0530

    Article carousel section on home page

--- app/routes/($lang)/index.jsx
+++ app/routes/($lang)/index.jsx
@@ -5,6 +5,7 @@ import {
   ProductSwimlane,
   Hero,
   KeyFeatures,
+  ArticleCarousel,
   Carousel,
   AnnouncementBar,
   ImageWithText,
@@ -19,6 +20,7 @@ import { getHeroPlaceholder } from '~/lib/placeholders';
 import { AnalyticsPageType } from '@shopify/hydrogen';
 import ImageWithTextStyles from '~/components/ImageWithText/ImageWithText.css';
 import KeyFeaturesStyles from '~/components/KeyFeatures/KeyFeatures.css';
+import ArticleCarouselStyles from '~/components/ArticleCarousel/ArticleCarousel.css';
 import CarouselStyles from '~/components/Carousel/Carousel.css';
 import ProductSwimlaneStyles from '~/components/ProductSwimlane/ProductSwimlane.css';
 import VideoPlayerStyles from '~/components/VideoPlayer/VideoPlayer.css';
@@ -32,6 +34,7 @@ export const links = () => {
   return [
     {rel: 'stylesheet', href: ImageWithTextStyles},
     {rel: 'stylesheet', href: KeyFeaturesStyles},
+    {rel: 'stylesheet', href: ArticleCarouselStyles},
     {rel: 'stylesheet', href: CarouselStyles},
     {rel: 'stylesheet', href: ProductSwimlaneStyles},
     {rel: 'stylesheet', href: VideoPlayerStyles},
@@ -81,6 +84,12 @@ export async function loader({ params, context }) {
   const features = page.metafields.find(item => {
     if (item !== null) {
       return item.key == "features"
+    }
+  })
+
+  const articles = page.metafields.find(item => {
+    if (item !== null) {
+      return item.key == "article_carousel"
     }
   })
 
@@ -172,6 +181,7 @@ export async function loader({ params, context }) {
   return defer({
     primaryHero: JSON.parse(hero?.value),
     features: features && JSON.parse(features?.value),
+    articles: articles && JSON.parse(articles?.value),
     goodbye: goodbye && JSON.parse(goodbye?.value),
     announcementbar: announcementbar && JSON.parse(announcementbar?.value),
     advancedFiltration: advancedFiltration && JSON.parse(advancedFiltration?.value),
@@ -199,6 +209,7 @@ export default function Homepage() {
     primaryHero,
     featuredProducts,
     features,
+    articles,
     goodbye,
     announcementbar,
     advancedFiltration,
@@ -231,6 +242,9 @@ export default function Homepage() {
 
       {features && (
         <KeyFeatures features={features} />)}
+
+      {articles && (
+        <ArticleCarousel articles={articles} />)}
 
       {goodbye && (
         <ImageWithText goodbye={goodbye} height="full" className="bg-white xl:flex"/>)}
@@ -288,6 +302,7 @@ const PAGE_QUERY = `#graphql
         identifiers: [
           { namespace: "global", key: "hero" }
           { namespace: "home", key: "features" }
+          { namespace: "home", key: "article_carousel" }
           { namespace: "home", key: "goodbye" }
           { namespace: "home", key: "announcement_bar" }
           { namespace: "global", key: "carousel" }
