19b700a90c51c27efeb0ed17c858472fd2dee615
darshika-dev <darshika@shoptrade.co>
2023-05-15 13:42:40 +0530

    Made minor changes in footer section

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -880,7 +880,7 @@ function Footer({footerCustomersMenu, footerInfoMenu, metafields}) {
           </div>
         </div>}
         {metafields !== "" && footerContactMetafields && footerContactMetafields.email_subscription.map((item, index) =>
-          (<div className="subscription-form" key={index}>
+          (<div className="subscription-form" key={index} id="email-subscription-form">
               <h3 className="footer-title mb-3.5">
                 {item.title}
               </h3>
