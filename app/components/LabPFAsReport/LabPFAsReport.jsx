d34fcd9ec2e0a7245a7f24c2cf9562fba05cecbb
darshika-dev <darshika@shoptrade.co>
2023-05-16 12:09:02 +0530

    Embed to Iframe

--- app/components/LabPFAsReport/LabPFAsReport.jsx
+++ app/components/LabPFAsReport/LabPFAsReport.jsx
@@ -5,8 +5,8 @@ export function LabPFAsReport({data}) {
         <section className='lab_pfas_report_section'>
             <Modal cancelLink={data?.page_link} classname="pdf_modal">
                 <div className="pdf_container">
-                    {/* <iframe src={data?.pdf_url}></iframe> */}
-                    <embed className="pdf_viewer" src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${data?.pdf_url}`} width="500" height="375" />
+                    <iframe src={data?.pdf_url}></iframe>
+                    {/* <embed className="pdf_viewer" src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${data?.pdf_url}`} width="500" height="375" /> */}
                 </div>
             </Modal>
         </section>
