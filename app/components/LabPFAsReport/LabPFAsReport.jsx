b75a295607733ff8d9af02c54505af65c8b1c657
darshika-dev <darshika@shoptrade.co>
2023-05-18 17:34:29 +0530

    dropbox iframe to embed switch

--- app/components/LabPFAsReport/LabPFAsReport.jsx
+++ app/components/LabPFAsReport/LabPFAsReport.jsx
@@ -5,7 +5,7 @@ const LabPFAsReport = ({data}) => {
     return <section className='lab_pfas_report_section'>
         <Modal cancelLink={data?.page_link} classname="pdf_modal">
             <div className="pdf_container">
-                <iframe src={data?.pdf_url.replace("?dl=0","?raw=1")} frameborder="0" allowfullscreen></iframe>
+                <embed className="pdf_viewer" src={data?.pdf_url.replace("?dl=0","?raw=1")} />
             </div>
         </Modal>
     </section>
