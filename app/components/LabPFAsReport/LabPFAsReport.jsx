939c8860965d69e331447d0b4509623f53baeebc
darshika-dev <darshika@shoptrade.co>
2023-05-18 18:49:19 +0530

    Used google docs to render google drive pdf url

--- app/components/LabPFAsReport/LabPFAsReport.jsx
+++ app/components/LabPFAsReport/LabPFAsReport.jsx
@@ -1,10 +1,14 @@
 import { Modal } from '~/components';
 
 const LabPFAsReport = ({data}) => {
+    let pdf_url = data.google_drive_url;
+    let capturedId = pdf_url.match(/\/d\/(.+)\//);
+    let gdrive_pdf_viewer = `https://docs.google.com/viewer?srcid=${capturedId[1]}&pid=explorer&efh=false&a=v&chrome=false&embedded=true`;
+
     return <section className='lab_pfas_report_section'>
-        <Modal cancelLink={data?.page_link} classname="pdf_modal">
+        <Modal cancelLink={data?.redirect_page_link} classname="pdf_modal">
             <div className="pdf_container">
-                <iframe src="https://docs.google.com/viewer?srcid=1HxY-Bs0Yz7OzhoP9ugx8sOgfiPm3uKjA&pid=explorer&efh=false&a=v&chrome=false&embedded=true" width="580px" height="480px"></iframe>
+                <iframe src={gdrive_pdf_viewer}></iframe>
             </div>
         </Modal>
     </section>
