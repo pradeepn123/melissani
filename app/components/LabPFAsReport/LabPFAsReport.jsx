e2c1df159c064cce5e972a24457c7ecab2053ff1
darshika-dev <darshika@shoptrade.co>
2023-05-18 17:40:33 +0530

    Dropbox embed with app key

--- app/components/LabPFAsReport/LabPFAsReport.jsx
+++ app/components/LabPFAsReport/LabPFAsReport.jsx
@@ -5,7 +5,10 @@ const LabPFAsReport = ({data}) => {
     return <section className='lab_pfas_report_section'>
         <Modal cancelLink={data?.page_link} classname="pdf_modal">
             <div className="pdf_container">
-                <embed className="pdf_viewer" src={data?.pdf_url.replace("?dl=0","?raw=1")} />
+                <a href={data?.pdf_url.replace("?dl=0","?raw=1")}
+                    class="dropbox-embed"
+                    data-height="300px"
+                    data-width="600px"></a>
             </div>
         </Modal>
     </section>
