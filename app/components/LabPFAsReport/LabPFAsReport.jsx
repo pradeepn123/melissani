0e7882bb7626b9167d4ed213cd53ed6af267509f
darshika-dev <darshika@shoptrade.co>
2023-05-18 17:27:45 +0530

    View pdf using Dropbox embed

--- app/components/LabPFAsReport/LabPFAsReport.jsx
+++ app/components/LabPFAsReport/LabPFAsReport.jsx
@@ -5,7 +5,7 @@ const LabPFAsReport = ({data}) => {
     return <section className='lab_pfas_report_section'>
         <Modal cancelLink={data?.page_link} classname="pdf_modal">
             <div className="pdf_container">
-                <embed className="pdf_viewer" src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${data?.pdf_url}`} />
+                <iframe src={data?.pdf_url.replace("?dl=0","?raw=1")} frameborder="0" allowfullscreen></iframe>
             </div>
         </Modal>
     </section>
