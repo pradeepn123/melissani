8eeb186b2c00daea06dbefa4ee3bf8b97dc504f1
darshika-dev <darshika@shoptrade.co>
2023-07-19 19:36:24 +0530

    Minor change

--- app/components/TwoSidedImageWithText/TwoSidedImageWithText.jsx
+++ app/components/TwoSidedImageWithText/TwoSidedImageWithText.jsx
@@ -1,6 +1,6 @@
 
 export function TwoSidedImageWithText({ twoSidedContent, className }) {
-  return <section className={`w-full gap-4 md:gap-0 items-center ${className && className}`}>
+  return <section className={`w-full gap-4 md:gap-0 lg:gap-0 items-center ${className && className}`}>
     {twoSidedContent && <>
       {twoSidedContent.map((content, index) => (
         <div className="individual-side-content" key={index}>
